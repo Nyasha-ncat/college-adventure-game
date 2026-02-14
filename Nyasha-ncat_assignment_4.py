029b6ecdd45aa24f8bab441464a7f24869aa85d3
Nyasha-ncat <ntchimombe@aggies.ncat.edu>
2026-02-13 20:21:47 -0500

    Beginning of program, added header

---
+++ Nyasha-ncat_assignment_4.py
@@ -0,0 +1,9 @@
+"""
+COMP 163 
+Assignment: Chapter 4 - College Life Adventure Game
+Name: Nyasha Chimombe
+GitHub Username: Nyasha-ncat
+Date: February 13, 2026
+Description: A college simulation game where the player manages their first semester.
+             The goal is to balance academics, social life, and stress to reach graduation.
+"""
